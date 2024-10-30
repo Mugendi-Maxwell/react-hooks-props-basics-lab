6e2653f31e5c78855d36891f577513ddb1efe4df
Maxi-arch <mxmu025@gmail.com>
2024-10-30 19:21:50 +0300

    1stcommit

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,28 @@
 import React from "react";
 
-function About() {
-  return (
+function About(props) {
+  const bio = props.bio;
+  if (bio) {
+    return(
+
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>I made this!</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
-    </div>
-  );
+      <h2>Links</h2>
+      <a href={props.github}>{props.github}</a>
+      <a href={props.linkedin}>{props.linkedin}</a>
+    </div>);
+  } else  {
+    return (
+      <div id="about">
+        <h2>About Me</h2>
+        <p> </p>
+        <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      </div>
+    );
+  }
 }
+
 
 export default About;
